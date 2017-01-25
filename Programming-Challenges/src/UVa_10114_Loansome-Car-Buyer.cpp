774e29c1270a5d75d0629dbe02d37ebe347531e4
Tianyi <twang006@e.ntu.edu.sg>
2017-01-25 23:14:32 +0800

    10114 done

--- Programming-Challenges/src/UVa_10114_Loansome-Car-Buyer.cpp
+++ Programming-Challenges/src/UVa_10114_Loansome-Car-Buyer.cpp
@@ -7,44 +7,38 @@ https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=1
 int main()
 {
 	int duration, dp_record;
-	double down, loan;
+	double down, loan, dp[101];
 
-	while (scanf("%d %lf %lf %d", 
-		&duration, &down, &loan, &dp_record), duration >= 0)
-	{
-		double owes = loan, worth = loan;
-		int result_month = 0;
-		int month = 0;
+	freopen("input.txt", "r", stdin);
+	freopen("output_mine.txt", "w", stdout);
 
-		// Begin the iteration on duration
-		while(duration--)
-		{	
-			double current_dp;
-			int current_month = 0;
+	// Outter loop for cases
+	while (scanf("%d %lf %lf %d",
+		&duration, &down, &loan, &dp_record), duration >= 0) {	// Get the inputs
 
-			if (month == 0)
-			{
-				scanf("%*d %lf", &current_month, &current_dp);
-				worth -= worth*(1 - current_dp);
-			}
-			else
-			{
-
-			}
-
-			owes -= down;	// down the owes		
-			
-
-			result_month++;
-			month++;
-
-			if (owes < worth)
-				break;
-								
+		int m; double p;
+		while (dp_record--) {
+			scanf("%d %lf", &m, &p);
+			for (int i = m; i < 101; i++)	// Put all needed dp values into an array
+				dp[i] = p;
 		}
 
-		// Output each case
-		result_month > 1 ? printf("%d months\n", result_month) : printf("%d month\n", result_month);
+		int result = 0;
+		double worth = (loan + down)*(1.0 - dp[0]);
+		double monthly = loan / duration;
+		double owes = loan;
+
+		while (owes > worth)
+		{
+			result++;
+			owes -= monthly;
+			worth = worth*(1.0 - dp[result]);
+		}
+
+		if (result != 1)
+			printf("%d months\n", result);
+		else
+			printf("%d month\n", result);
 	}
 
 	return 0;
